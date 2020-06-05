0a8a92ed99f85ea97841dcb7814dd0ae0a57775d
glennpaulmabao <64250849+glennpaulmabao@users.noreply.github.com>
2020-06-05 16:44:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,5 +20,17 @@
         }
     ?>
     </div>
+
+    <div class="gallery">
+    <?php
+        $files = glob("./img/*.*");
+        for ($i=1; $i<count($files); $i++)
+        {
+        $num = $files[$i];
+        	print $num."<br />";
+        echo '<img src="'.$num.'" alt="random image" />'."<br /><br />";
+        }
+    ?>
+    </div>
 </body>
 </html>
